d5fd793be8525cd86642965d2d175fd3a31b04f0
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2021-10-06 18:45:03 +0530

    hope to be final

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,18 +13,17 @@ class ProductController extends Controller
     function addProduct(Request $request)
     {
     	$product = new Product;
-        return($request->name);
-    	// $product->name = $request->name;
-    	// $product->description = $request->description;
-    	// $product->price = $request->price;
-     //    $uploadedFileUrl = Cloudinary::upload($request->file('file')->getRealPath(),
-     //        array("folder" => "ecom-dashboard", "overwrite" => TRUE, "resource_type" => "image"))->getSecurePath();
-     //    // return($uploadedFileUrl);
-    	// // $product->file_path=$request->file('file')->store('products');
-     //    $product->file_path=$uploadedFileUrl;
-    	// $product->save();
-     //    $result = array("status"=>"success");
-    	// return json_encode($result);
+    	$product->name = $request->name;
+    	$product->description = $request->description;
+    	$product->price = $request->price;
+        $uploadedFileUrl = Cloudinary::upload($request->file('file')->getRealPath(),
+            array("folder" => "ecom-dashboard", "overwrite" => TRUE, "resource_type" => "image"))->getSecurePath();
+        // return($uploadedFileUrl);
+    	// $product->file_path=$request->file('file')->store('products');
+        $product->file_path=$uploadedFileUrl;
+    	$product->save();
+        $result = array("status"=>"success");
+    	return json_encode($result);
 
     }
 
